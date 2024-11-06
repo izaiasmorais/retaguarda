3c9e4ab91c7038bf5f319ae195d2123e639af2f0
izaiasmorais <izaiaslima356@gmail.com>
2024-11-06 18:27:32 -0300

    build: set up vite ts app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Header } from "./components/header";
+import { Sidebar } from "./components/sidebar";
+
+export default function App() {
+	return (
+		<div className="xl:grid xl:grid-cols-app min-h-screen">
+			<Sidebar />
+
+			<main className="xl:col-start-2 max-w-[100vw] flex flex-col h-screen">
+				<Header />
+
+				<div className="p-4 flex-grow overflow-auto">
+					<div>Conteúdo</div>
+				</div>
+			</main>
+		</div>
+	);
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,5 @@
+export function Header() {
+	return (
+		<header className="flex w-full justify-between p-4 border-b border-muted bg-slate-50"></header>
+	);
+}
