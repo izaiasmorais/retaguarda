dc747d7bc7eb3a0d9ce615de3114b7b11d920fb1
izaiasmorais <izaiaslima356@gmail.com>
2024-11-06 19:15:42 -0300

    fix: unused declarations

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import { SidebarItem } from "./sidebar-item";
 import {
 	Banknote,
-	ChevronRight,
 	LayoutDashboard,
 	Minus,
 	Receipt,
@@ -15,15 +13,8 @@ import {
 } from "lucide-react";
 
 export function Sidebar() {
-	const [isOpen, setIsOpen] = useState<"open" | "closed">("closed");
-
-	function handleToggleCollapsible() {
-		setIsOpen((prev) => (prev === "open" ? "closed" : "open"));
-	}
-
 	return (
 		<div
-			data-state={isOpen}
 			className="border-b bg-background data-[state=open]:bottom-0 xl:data-[state=closed]:bottom-0 left-0 top-0
 			right-0 flex flex-col p-4 fixed z-20 xl:right-auto xl:w-64 xl:border-r gap-6 border-muted
 			data-[state=open]:h-screen xl:data-[state=open]:h-screen xl:h-auto"
